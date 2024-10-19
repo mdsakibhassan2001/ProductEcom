664b1ceac85e315d6bc44bfe64803d54c77318d1
Md Sakib Hassan <74108908+mdsakibhassan2001@users.noreply.github.com>
2024-10-19 18:47:39 +0600

    etc

--- product-ecom-frontend/src/Component/Home/MainSlider.js
+++ product-ecom-frontend/src/Component/Home/MainSlider.js
@@ -1,35 +1,34 @@
+"use client";
 import React from 'react'
 import Slider from 'react-slick';
 import style from './css/mainSlider.module.css'
+import oil from '../../../public/assets/1710569991103_Olive-Oils-Land.webp'
+import roxanne from '../../../public/assets/Roxanne.webp'
+import cold_care from '../../../public/assets/1710570260155_Gold-Care.webp'
+import { CiCircleChevRight,CiCircleChevLeft } from "react-icons/ci";
+import Image from 'next/image';
 export default function MainSlider() {
   var settings = {
     dots: true,
     infinite: true,
+    autoplay: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    cssEase: "linear",
   };
   return (
     <div className={style.wrapper}>
         <div className={style.holder}>
         <Slider {...settings}>
           <div>
-            <h3>1</h3>
+            <Image height="auto" width='100%'  alt='oil' src={oil}/>
           </div>
           <div>
-            <h3>2</h3>
+            <Image alt='roxanne' height="auto" width='100%' src={roxanne}/>
           </div>
           <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
+            <Image alt='cold_care' height="auto" width='100%' src={cold_care}/>
           </div>
         </Slider>
         </div>
