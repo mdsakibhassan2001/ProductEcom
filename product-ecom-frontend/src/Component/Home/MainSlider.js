0ea1d9770f4d092ffb7b92ff5b09e51ee1d9cb39
Md Sakib Hassan <74108908+mdsakibhassan2001@users.noreply.github.com>
2024-11-20 00:20:07 +0600

    some work

--- product-ecom-frontend/src/Component/Home/MainSlider.js
+++ product-ecom-frontend/src/Component/Home/MainSlider.js
@@ -1,21 +1,21 @@
 "use client";
-import React from 'react'
-import Slider from 'react-slick';
-import style from './css/mainSlider.module.css'
-import Image from 'next/image';
+import React from "react";
+import Slider from "react-slick";
+import style from "./css/mainSlider.module.css";
+import Image from "next/image";
 export default function MainSlider() {
-  const mainSliderContent=[
-    {img:"/assets/slider/slider1.jpg"},
-    {img:"/assets/slider/slider2.jpg"},
-    {img:"/assets/slider/slider3.jpg"},
-    {img:"/assets/slider/slider4.jpg"},
-    {img:"/assets/slider/slider5.jpg"},
-    {img:"/assets/slider/slider6.jpg"},
-    {img:"/assets/slider/slider7.jpg"},
-    {img:"/assets/slider/slider8.jpg"},
-    {img:"/assets/slider/slider9.jpg"},
-    {img:"/assets/slider/slider10.jpg"},
-  ]
+  const mainSliderContent = [
+    { img: "/assets/slider/slider1.png" },
+    { img: "/assets/slider/slider2.png" },
+    { img: "/assets/slider/slider3.png" },
+    { img: "/assets/slider/slider4.png" },
+    { img: "/assets/slider/slider5.png" },
+    { img: "/assets/slider/slider6.png" },
+    { img: "/assets/slider/slider7.png" },
+    { img: "/assets/slider/slider8.png" },
+    { img: "/assets/slider/slider9.png" },
+    { img: "/assets/slider/slider10.png" },
+  ];
   var settings = {
     dots: true,
     infinite: true,
@@ -27,18 +27,17 @@ export default function MainSlider() {
   };
   return (
     <div className={style.wrapper}>
-        <div className={style.holder}>
+      <div className={style.holder}>
         <Slider {...settings}>
-          {
-            mainSliderContent.map((item,index)=>{
-             return <div key={index} className={style.image_wrapper}>
-                  <Image fill  alt='oil' src={item.img}/>
+          {mainSliderContent.map((item, index) => {
+            return (
+              <div key={index} className={style.image_wrapper}>
+                <Image fill alt="oil" src={item.img} />
               </div>
-            })
-          }
-         
+            );
+          })}
         </Slider>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
